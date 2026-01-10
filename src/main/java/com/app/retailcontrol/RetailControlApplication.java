ce6ff45d6eaa9c8349d9b17627071989344372c7
Thomas Serna <tasernas@eafit.edu.co>
2026-01-10 14:36:57 -0500

    Initial project structure

---
+++ src/main/java/com/app/retailcontrol/RetailControlApplication.java
@@ -0,0 +1,13 @@
+package com.app.retailcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailControlApplication.class, args);
+    }
+
+}
